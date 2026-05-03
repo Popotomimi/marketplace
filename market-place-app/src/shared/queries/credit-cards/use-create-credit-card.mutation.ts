3e07f77ee06489a263084856af1733ffc512447a
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2026-05-03 15:01:27 -0300

    Create Service and Query

---
+++ market-place-app/src/shared/queries/credit-cards/use-create-credit-card.mutation.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CreateCreditCardRequestParams } from "../../interfaces/http/create-credit-card";
+import { createCreditCard } from "../../services/credit-card.service";
+import { Toast } from "toastify-react-native";
+
+export const useCreateCreditCardMutation = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (creditCardData: CreateCreditCardRequestParams) =>
+      createCreditCard(creditCardData),
+    onSuccess: (response) => {
+      Toast.success(response.message ?? "Cartão criado com sucesso");
+      queryClient.cancelQueries({
+        queryKey: ["credit-cards"],
+      });
+    },
+  });
+
+  return mutation;
+};
--- market-place-app/src/shared/services/credit-card.service.ts
+++ market-place-app/src/shared/services/credit-card.service.ts
@@ -1,8 +1,23 @@
 import { marketPlaceApiClient } from "../api/market-place";
-import { GetCreditCardsResponse } from "../interfaces/http/credit-card";
+import { CreditCard } from "../interfaces/credit-card";
+import {
+  CreateCreditCardRequestParams,
+  CreateCreditCardResponse,
+} from "../interfaces/http/create-credit-card";
 
 export const getCreditCards = async () => {
   const { data } =
-    await marketPlaceApiClient.get<GetCreditCardsResponse[]>("/creadit-cards");
+    await marketPlaceApiClient.get<CreditCard[]>("/creadit-cards");
+  return data;
+};
+
+export const createCreditCard = async (
+  creditCardData: CreateCreditCardRequestParams,
+) => {
+  const { data } = await marketPlaceApiClient.post<CreateCreditCardResponse>(
+    "/creadit-cards",
+    creditCardData,
+  );
+
   return data;
 };
