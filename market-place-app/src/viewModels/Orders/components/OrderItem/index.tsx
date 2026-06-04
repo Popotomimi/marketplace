ba84c2e2c0ec78b59e9caecac4e282cebc5d88dc
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2026-06-04 14:47:25 -0300

    Add formart date-fns to orders and update UI and Ux

--- market-place-app/src/viewModels/Orders/components/OrderItem/index.tsx
+++ market-place-app/src/viewModels/Orders/components/OrderItem/index.tsx
@@ -2,6 +2,8 @@ import { Image, Text, View } from "react-native";
 import { OrderInterface } from "../../../../shared/interfaces/order";
 import { FC } from "react";
 import { BuildImageUrl } from "../../../../shared/helpers/buildImageUrl";
+import { format } from "date-fns/format";
+import { AppPriceText } from "../../../../shared/components/AppPriceText";
 
 interface OrderItemParams {
   order: OrderInterface;
@@ -26,14 +28,26 @@ export const OrderItem: FC<OrderItemParams> = ({ order }) => {
             {order.productName}
           </Text>
 
-          <Text>{order.createdAt.toString()}</Text>
+          <Text className="text-sm text-gray-600">
+            {format(order.createdAt, "dd/MM/yyyy")}
+          </Text>
         </View>
 
-        <Text>
-          {order.quantity} - ${order.totalPrice.toFixed(2)}
-        </Text>
+        <View className="flex-row mb-2">
+          <Text className="text-sm text-gray-600 mr-1">
+            {order.quantity} {order.quantity > 1 ? "Unidades" : "Unidade"}{" "}
+            •{" "}
+          </Text>
+          <AppPriceText
+            value={order.totalPrice}
+            classNameCurrency="text-sm text-gray-600"
+            classNameValue="text-sm text-gray-600"
+          />
+        </View>
 
-        <Text>{order.creditCard.maskedNumber}</Text>
+        <Text className="text-sm text-gray-600">
+          Cartão final {order.creditCard.maskedNumber.slice(-4)}
+        </Text>
       </View>
     </View>
   );
